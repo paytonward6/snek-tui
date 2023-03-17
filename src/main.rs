3f17edbb59f034928d3524eb7af11353848f9927
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2023-03-17 15:18:35 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::{io, thread, time::Duration};
+
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+mod Model {
+    use std::collections::LinkedList;
+
+    struct Snake {
+        // Using u32 as placeholder for now,
+        // will probably be a tui::Block
+        body: LinkedList<u32>,
+        direction: Direction,
+        position: Position,
+    }
+
+    struct State {
+        snake: Snake,
+        time_elapsed: u32,
+    }
+
+    struct Position {
+        x: u32,
+        y: u32,
+    }
+
+    enum Direction {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
